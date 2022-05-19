58a9505dafdf6e44e8d61a366f2151a03fd21e3d
Edouard Tisserant <edouard.tisserant@gmail.com>
2022-05-19 18:08:47 +0200

    windows installer: force CanFestival build to use source code already checked out rather than re-doing it in anotrher directory.

--- win32.mk
+++ win32.mk
@@ -5,17 +5,19 @@ OWN_PROJECTS_EX=CanFestival-3
 ide_targets_from_dist: canfestival
 	touch $@
 
-CFbuild = installer/CanFestival-3
-canfestival: $(msysdir)/.stamp
-	rm -rf $(CFbuild)
-	$(call get_src_hg,$(CFbuild))
-	cd $(CFbuild); \
+CanFestival-3_dir = installer/CanFestival-3
+CanFestival-3: $(CanFestival-3_dir)/.stamp
+$(CanFestival-3_dir)/.stamp: sources/CanFestival-3_src | installer
+canfestival: $(CanFestival-3_dir)/.stamp
+	rm -rf $(CanFestival-3_dir)
+	cp -a sources/CanFestival-3 $(CanFestival-3_dir)
+	cd $(CanFestival-3_dir); \
 	./configure --can=tcp_win32 \
 				--cc=$(CC) \
 				--cxx=$(CXX) \
 				--target=win32 \
 				--wx=0
-	$(MAKE) -C $(CFbuild)
-	cd $(CFbuild); find . -name "*.o" -exec rm {} ';' #remove object files only
+	$(MAKE) -C $(CanFestival-3_dir)
+	cd $(CanFestival-3_dir); find . -name "*.o" -exec rm {} ';' #remove object files only
 	touch $@
 
