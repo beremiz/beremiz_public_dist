f53c18af88b2a1e2ed4678d9b2336ee18d9df622
Edouard Tisserant <edouard.tisserant@gmail.com>
2022-05-20 18:50:40 +0200

    ...

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -4,6 +4,6 @@ CONTAINER=beremiz_public_builder_current
 
 docker start $CONTAINER 
 echo exec docker with $*
-docker exec $CONTAINER bash -c build $*
+docker exec $CONTAINER bash -c "build $*"
 docker stop $CONTAINER
 
