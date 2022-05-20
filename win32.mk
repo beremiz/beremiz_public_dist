7712e6e3c28db642b1b2e8ea7c885a95c35035e9
Edouard Tisserant <edouard.tisserant@gmail.com>
2022-05-20 08:04:52 +0200

    Rename CanFestival-3 repo into canfestival. CamelCase and '-' in repo name are silly.

--- win32.mk
+++ win32.mk
@@ -1,23 +1,23 @@
 # Win32 only distribution
 
-OWN_PROJECTS_EX=CanFestival-3
+OWN_PROJECTS_EX=canfestival
 
 ide_targets_from_dist: canfestival
 	touch $@
 
-CanFestival-3_dir = installer/CanFestival-3
-CanFestival-3: $(CanFestival-3_dir)/.stamp
-$(CanFestival-3_dir)/.stamp: sources/CanFestival-3_src | installer
-canfestival: $(CanFestival-3_dir)/.stamp
-	rm -rf $(CanFestival-3_dir)
-	cp -a sources/CanFestival-3 $(CanFestival-3_dir)
-	cd $(CanFestival-3_dir); \
+canfestival_dir = installer/canfestival
+canfestival: $(canfestival_dir)/.stamp
+$(canfestival_dir)/.stamp: sources/canfestival_src | installer
+canfestival: $(canfestival_dir)/.stamp
+	rm -rf $(canfestival_dir)
+	cp -a sources/canfestival $(canfestival_dir)
+	cd $(canfestival_dir); \
 	./configure --can=tcp_win32 \
 				--cc=$(CC) \
 				--cxx=$(CXX) \
 				--target=win32 \
 				--wx=0
-	$(MAKE) -C $(CanFestival-3_dir)
-	cd $(CanFestival-3_dir); find . -name "*.o" -exec rm {} ';' #remove object files only
+	$(MAKE) -C $(canfestival_dir)
+	cd $(canfestival_dir); find . -name "*.o" -exec rm {} ';' #remove object files only
 	touch $@
 
